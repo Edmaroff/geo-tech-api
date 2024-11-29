39090d1d0a7c01e403b67cc2bc4f7ad006a640ea
Eduard Tatarnikov <108517865+Edmaroff@users.noreply.github.com>
2024-11-29 17:21:45 +0800

    add load env

--- src/config/settings.py
+++ src/config/settings.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY", "django-5bxkrm0u&$a")
 DEBUG = os.getenv("DEBUG", "True") == "True"
